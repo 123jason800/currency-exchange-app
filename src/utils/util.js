d706f3e0a6c1756dd793c501b6ac890cf7e8df18
jasonouyang8000 <jianjun.ouyang@lacity.org>
2021-01-09 18:12:38 -0800

    updates to page

--- src/utils/util.js
+++ src/utils/util.js
@@ -15,14 +15,22 @@ export const handleRes = response => {
         if (response.every(response => response.ok !== true)) {
             throw Error('404 or 500 error');
         }
+        
         return Promise.all(response.map(response => response.json()));
     }
 
 }
 
 
+
 export const getPreviousDate = () => {
-    return moment().subtract(1, 'days').format('YYYY-MM-DD');
+    const currentHour = parseInt(moment().format("HH"));
+    // Account for Stock Open Hours
+    if (currentHour >= 5 &&  currentHour <= 16) {
+        return moment().subtract(1, 'days').format('YYYY-MM-DD');
+    }    
+    return moment().subtract(2, 'days').format('YYYY-MM-DD');
+    
 }
 
 export const calculateRate = (rate,rateYesterday) => {
